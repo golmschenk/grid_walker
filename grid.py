256cf80fc8f1864b9133f9ebadaa91358ee151e4
Greg Olmschenk <greg@olmschenk.com>
2022-12-14 00:20:15 -0500

    Add colors

--- grid.py
+++ grid.py
@@ -1,6 +1,7 @@
 import os
 
 import numpy as np
+from rich import print
 
 from direction import Direction
 
@@ -15,16 +16,28 @@ class GridWalker:
         self.player_symbol = '&'
         self.explored_symbol = '.'
         self.unexplored_symbol = '#'
+        self.explored_count = 0
         self.explored_grid: np.ndarray = np.full([self.height, self.width], fill_value=self.unexplored_symbol)
         self.explored_grid[self.y, self.x] = self.portal_symbol
 
     def print_grid(self):
         os.system('cls' if os.name == 'nt' else 'clear')
-        print_grid = self.explored_grid.copy()
-        print_grid[self.y, self.x] = self.player_symbol
-        for row in print_grid:
-            for cell in row:
-                print(cell, end='')
+        for row_index, row in enumerate(self.explored_grid):
+            for column_index, cell in enumerate(row):
+                background_color = 'grey'
+                foreground_color = 'black'
+                character = cell
+                if row_index == self.y and column_index == self.x:
+                    foreground_color = 'orange_red1'
+                    character = self.player_symbol
+                if character == self.portal_symbol:
+                    background_color = 'purple'
+                elif character == '1':
+                    background_color = 'red'
+                elif character != self.unexplored_symbol:
+                    background_color = 'green'
+                cell_display_string = f'[{foreground_color} on {background_color}]{character}[/]'
+                print(cell_display_string, end='')
             print('')
 
     def move_and_print(self, direction: Direction):
@@ -37,5 +50,9 @@ class GridWalker:
         elif direction == Direction.LEFT:
             self.x -= 1
         if self.explored_grid[self.y, self.x] == self.unexplored_symbol:
-            self.explored_grid[self.y, self.x] = self.explored_symbol
+            self.explored_count += 1
+            if self.explored_count < 10:
+                self.explored_grid[self.y, self.x] = str(self.explored_count)
+            else:
+                self.explored_grid[self.y, self.x] = '.'
         self.print_grid()
