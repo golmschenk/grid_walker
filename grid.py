ef0854a05d3bfa93a7a71d1a054699266fac8006
Greg Olmschenk <greg@olmschenk.com>
2022-12-14 19:01:28 -0500

    Rename player position variables

--- grid.py
+++ grid.py
@@ -10,15 +10,15 @@ class GridWalker:
     def __init__(self):
         self.width: int = 15
         self.height: int = 15
-        self.x: int = self.width // 2
-        self.y: int = self.height // 2
+        self.player_x: int = self.width // 2
+        self.player_y: int = self.height // 2
         self.portal_symbol = '&'
         self.player_symbol = '@'
-        self.explored_symbol = '.'
+        self.explored_overflow_symbol = '.'
         self.unexplored_symbol = '#'
         self.explored_count = 0
         self.explored_grid: np.ndarray = np.full([self.height, self.width], fill_value=self.unexplored_symbol)
-        self.explored_grid[self.y, self.x] = self.portal_symbol
+        self.explored_grid[self.player_y, self.player_x] = self.portal_symbol
 
     def print_grid(self):
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -27,7 +27,7 @@ class GridWalker:
                 background_color = 'grey'
                 foreground_color = 'black'
                 character = cell
-                if row_index == self.y and column_index == self.x:
+                if row_index == self.player_y and column_index == self.player_x:
                     foreground_color = 'orange_red1'
                     character = self.player_symbol
                 if character == self.portal_symbol:
@@ -42,17 +42,17 @@ class GridWalker:
 
     def move_and_print(self, direction: Direction):
         if direction == Direction.UP:
-            self.y -= 1
+            self.player_y -= 1
         elif direction == Direction.DOWN:
-            self.y += 1
+            self.player_y += 1
         elif direction == Direction.RIGHT:
-            self.x += 1
+            self.player_x += 1
         elif direction == Direction.LEFT:
-            self.x -= 1
-        if self.explored_grid[self.y, self.x] == self.unexplored_symbol:
+            self.player_x -= 1
+        if self.explored_grid[self.player_y, self.player_x] == self.unexplored_symbol:
             self.explored_count += 1
             if self.explored_count < 10:
-                self.explored_grid[self.y, self.x] = str(self.explored_count)
+                self.explored_grid[self.player_y, self.player_x] = str(self.explored_count)
             else:
-                self.explored_grid[self.y, self.x] = '.'
+                self.explored_grid[self.player_y, self.player_x] = self.explored_overflow_symbol
         self.print_grid()
